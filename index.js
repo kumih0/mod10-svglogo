ce0b9a5f44a0a7b0c48fa1f94adbfb3ae2b6ad80
KT <132851569+kumih0@users.noreply.github.com>
2023-06-13 18:00:57 -0500

    adding questions to array index js

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs =require('fs');
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message:'What is your logo text?',
+        
+    },
+    {
+        
+    }
