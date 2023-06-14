5423f82758c9f471b8271e3b39bcab2d35166efb
KT <132851569+kumih0@users.noreply.github.com>
2023-06-14 17:04:06 -0500

    updating to match inquirer syntax

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-const fs =require('fs');
-const inquirer = require('inquirer');
-const Shapes = require('./lib/shapes');
-const SVG = require('./lib/svg');
+import fs from 'fs';
+import inquirer from 'inquirer';
+import { Square, Triangle, Circle } from './lib/shapes';
+import { SVG } from './lib/svg';
 
 const questions = [
     {
         type: 'input',
         name: 'text',
-        message:'What is your logo text?',
-        
+        message: 'What is your logo text?',
+
     },
     {
         type: 'input',
         name: 'textcolor',
-        message:'What is the text color for your logo?',
+        message: 'What is the text color for your logo?',
 
     },
     {
         type: 'list',
         name: 'shape',
-        message:'What is the shape for your logo?',
-        choices: ['square', 'circle', 'triangle'], 
+        message: 'What is the shape for your logo?',
+        choices: ['square', 'circle', 'triangle'],
     },
     {
         type: 'input',
         name: 'shapecolor',
-        message:'What is the shape color for your logo?',
+        message: 'What is the shape color for your logo?',
     },
 ]
 
- inquirer.prompt(questions).then(answers, function({text, textcolor, shape, shapecolor}) {
-        const svg = new SVG();
-        svg.setText(text, textcolor);
-        const shape = new Shapes();
-        shape.setColor(shapecolor);
-        svg.setShape(shape);
-        fs.writeFile('logo.svg', svg.render());
-    })
+inquirer.prompt(questions).then(({ text, textcolor, shape, shapecolor }) => {
+    let shapetype;
+    switch (shape) {
+        case 'square':
+            shapetype = new Square();
+            break;
+        case 'circle':
+            shapetype = new Circle();
+            break;
+        case 'triangle':
+            shapetype = new Triangle();
+            break;
+    }
+    shapetype.setColor(shapecolor);
+
+    const svg = new SVG();
+    svg.setText(text, textcolor);
+    svg.setShape(shapetype);
+    fs.writeFile('logo.svg', svg.render());
+})
 
 // .then(function() {
 //     console.log(text, textcolor, shape, shapecolor);
